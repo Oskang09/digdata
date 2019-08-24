3cbf2d102cd4a573183d6fe6082b6504c0db2a93
devOska <developer.oskang@gmail.com>
2019-08-24 17:10:00 +0800

    Initialize Repository

---
+++ index.js
@@ -0,0 +1,39 @@
+const dig = function (object) {
+    if (!object) {
+        return null;
+    }
+
+    let args = Array.prototype.slice.call(arguments, 1);
+    if (typeof args[0] === 'string' && args[0].indexOf('.') !== -1) {
+        args = args[0].split('.');
+    }
+
+    if (Array.isArray(args[0])) {
+        args = args[0];
+    }
+
+    let target = object;
+    for (let index = 0; index < args.length; index += 1) {
+        const notation = args[index];
+
+        if (notation.indexOf('=') !== -1) {
+            const comparison = notation.split('=');
+            const key = comparison[0];
+            const value = comparison[1];
+            target = target.find((t) => t[key] == value);
+            if (!target) {
+                return null;
+            }
+        } else if (notation === '*' && Array.isArray(target)) {
+            return target.map((t) => dig(t, args.slice(index + 1)));
+        } else {
+            if (!target[notation]) {
+                return null;
+            }
+            target = target[notation];
+        }
+    }
+    return target;
+};
+
+module.exports = { dig };
---
+++ index.test.js
@@ -0,0 +1,60 @@
+const { dig } = require('./index');
+
+const object = {
+    owner: {
+        id: 14,
+        name: 'Jason',
+        age: 19
+    },
+    users: [
+        {
+            id: 1,
+            name: 'Oskang09',
+            age: 20,
+            email: 'developer.oskang@gmail.com',
+        },
+        {
+            id: 2,
+            name: 'Roger',
+            age: 24,
+        }
+    ]
+};
+
+test('Should mutliple arguments notation accepted. ', () => {
+    const res = dig(object, 'owner', 'name');
+    expect(res).toBe(object.owner.name);
+});
+
+test('Should array argument notation accepted.', () => {
+    const res = dig(object, [ 'owner', 'name' ]);
+    expect(res).toBe(object.owner.name);
+});
+
+test('Should dot string notation accepted.', () => {
+    const res = dig(object, 'owner.name');
+    expect(res).toBe(object.owner.name);
+});
+
+test('Should equal notation return object based on condition.', () => {
+    const res = dig(object, 'users.id=1');
+    expect(res).toBe(object.users[0]);
+});
+
+test('Should equal notation return null when target not exists.', () => {
+    const res = dig(object, 'users.id=3');
+    expect(res).toBe(null);
+});
+
+test('Should filter notation return specific key', () => {
+    const res = dig(object, 'users.*.name');
+    expect(res).toStrictEqual([ 'Oskang09', 'Roger' ]);
+});
+
+test('Should return null if dot notation not exists.', () => {
+    expect(dig(object, 'owner.name.id')).toBe(null);
+});
+
+test('Should return null if object is falsy value.', () => {
+    expect(dig(null, 'owner.name')).toBe(null);
+});
